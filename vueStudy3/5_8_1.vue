9722fbdb0381712528c9a8a7f6c032dc2b80dc45
Braveheart-qw <609830076@qq.com>
2020-09-06 21:55:36 +0800

    vue

---
+++ vueStudy3/5_8_1.vue
@@ -0,0 +1,21 @@
+
+<template>
+    此处存放dom结构如：
+    <p>hello</p>
+</template>
+
+<script>
+    module.exports = {
+        data: function () {
+            return {
+                js区域
+            }
+        }
+    }
+</script>
+
+<style>
+    p{
+        background: #fe0000;
+    }
+</style>
